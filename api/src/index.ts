4071c7bb3668fb7b484e86a0a5812ea48a6f0c4f
Trevor Sharp <sharp.trevor@me.com>
2022-07-29 20:35:49 +0000

    Remove feed id

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,31 +3,25 @@ import { spawn } from 'child_process';
 import express from 'express';
 import { z } from 'zod';
 
-const MAX_DOWNLOADS_PER_FEED = z
-  .string()
-  .regex(/^[0-9]+$/)
-  .transform((x) => parseInt(x))
-  .parse(process.env.MAX_DOWNLOADS_PER_FEED);
-
 const VIDEO_QUALITY = z
   .string()
   .regex(/^(2160|1440|1080).*$/)
   .transform((x) => parseInt(x.slice(0, 4)))
   .parse(process.env.VIDEO_QUALITY);
 
-const downloadVideos = (feedId: string, videoList: string[]): void => {
-  console.log(`Received Feed: ${feedId}`);
+const downloadVideos = (videoList: string[]): void => {
+  console.log(`Received New Video List`);
 
-  videoList.slice(0, Math.min(videoList.length, MAX_DOWNLOADS_PER_FEED)).forEach((videoId) => {
+  videoList.slice(0, Math.min(videoList.length, 2)).forEach((videoId) => {
     if (
-      !fs.existsSync(`/content/${feedId}/${videoId}.mp4`) &&
-      !fs.existsSync(`/content/${feedId}/${videoId}.temp.mp4`)
+      !fs.existsSync(`/content/${videoId}.mp4`) &&
+      !fs.existsSync(`/content/${videoId}.temp.mp4`)
     ) {
       console.log(`Starting Download: ${videoId}`);
 
       const videoDownloadProcess = spawn('sh', [
         './downloadVideos.sh',
-        `/content/${feedId}`,
+        `/content`,
         videoId,
         `${VIDEO_QUALITY}`,
       ]);
@@ -46,16 +40,11 @@ const port = 80;
 
 app.post('/', async (req, res) => {
   try {
-    const request = z
-      .object({
-        feedId: z.string().regex(/^[A-Z0-9_\-]{0,30}$/i),
-        videoIds: z.array(z.string().regex(/^[A-Z0-9_\-]{11}$/i)),
-      })
-      .safeParse(req.body);
+    const request = z.array(z.string().regex(/^[A-Z0-9_\-]{11}$/i)).safeParse(req.body);
 
     if (!request.success) return res.status(400).send();
 
-    downloadVideos(request.data.feedId, request.data.videoIds);
+    downloadVideos(request.data);
 
     res.status(200).send();
   } catch (error) {
@@ -63,14 +52,11 @@ app.post('/', async (req, res) => {
   }
 });
 
-app.get('/:feedId/:videoId', (req, res) => {
+app.get('/:videoId', (req, res) => {
   try {
-    const feedId = req.params.feedId;
     const videoId = req.params.videoId;
 
-    console.log(feedId, videoId);
-
-    const videoFilePath = `/content/${feedId}/${videoId}.mp4`;
+    const videoFilePath = `/content/${videoId}.mp4`;
 
     if (!fs.existsSync(videoFilePath)) return res.status(404).send();
 

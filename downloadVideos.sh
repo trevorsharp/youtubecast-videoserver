f0b685430934b4a5ad9dea3745ae7621761afa88
Trevor Sharp <sharp.trevor@me.com>
2022-07-30 20:01:56 +0000

    Add support for cookies.txt

--- downloadVideos.sh
+++ downloadVideos.sh
@@ -8,16 +8,32 @@ mkdir -p "$videoDirectory"
 
 touch "$videoDirectory/$videoId.temp"
 
-yt-dlp \
-    -f "bv[height<=${maxHeight}]" \
-    -S "height,ext" \
-    -o "$videoDirectory/%(id)s.video" \
-    "https://youtube.com/watch?v=$videoId"
+if [ -e cookies.txt ]
+then
+    yt-dlp \
+        -f "bv[height<=${maxHeight}]" \
+        -S "height,ext" \
+        -o "$videoDirectory/%(id)s.video" \
+        --cookies cookies.txt \
+        "https://youtube.com/watch?v=$videoId"
 
-yt-dlp \
-    -f "ba[ext=m4a]" \
-    -o "$videoDirectory/%(id)s.audio" \
-    "https://youtube.com/watch?v=$videoId"
+    yt-dlp \
+        -f "ba[ext=m4a]" \
+        -o "$videoDirectory/%(id)s.audio" \
+        --cookies cookies.txt \
+        "https://youtube.com/watch?v=$videoId"
+else
+    yt-dlp \
+        -f "bv[height<=${maxHeight}]" \
+        -S "height,ext" \
+        -o "$videoDirectory/%(id)s.video" \
+        "https://youtube.com/watch?v=$videoId"
+
+    yt-dlp \
+        -f "ba[ext=m4a]" \
+        -o "$videoDirectory/%(id)s.audio" \
+        "https://youtube.com/watch?v=$videoId"
+fi
 
 videoCodec=$(ffprobe \
     -v error \

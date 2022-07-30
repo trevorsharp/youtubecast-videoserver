291dbc7f9a994a231d1c84b26f730db78d8cdb7e
Trevor Sharp <sharp.trevor@me.com>
2022-07-30 02:32:06 +0000

    Skip re-encode if video is already h264

--- downloadVideos.sh
+++ downloadVideos.sh
@@ -15,6 +15,39 @@ yt-dlp \
     -o "$videoDirectory/%(id)s.%(ext)s" \
     $videoId
 
+videoCodec = $(ffprobe \
+    -v error \
+    -select_streams v:0 \
+    -show_entries stream=codec_name \
+    -of default=noprint_wrappers=1:nokey=1 \
+    $videoDirectory/$videoId.mkv)
+
+if [ "$videoCodec" = "h264" ]; then
+    ffmpeg \
+        -hide_banner \
+        -loglevel quiet \
+        -i "$videoDirectory/$videoId.mkv" \
+        -c:v copy \
+        -c:a copy \
+        -f hls \
+        -hls_playlist_type vod \
+        -hls_flags single_file \
+        $videoDirectory/$videoId.m3u8
+else
+    ffmpeg \
+        -hide_banner \
+        -loglevel quiet \
+        -i "$videoDirectory/$videoId.mkv" \
+        -c:v libx264 \
+        -c:a copy \
+        -preset veryfast \
+        -r 30 \
+        -f hls \
+        -hls_playlist_type vod \
+        -hls_flags single_file \
+        $videoDirectory/$videoId.m3u8
+fi
+
 ffmpeg \
     -hide_banner \
     -loglevel quiet \

fd39bed22ba21856e065a770c94533e1b7f3b91e
Trevor Sharp <sharp.trevor@me.com>
2023-07-16 01:06:50 +0000

    Fix no needed removal after moving files

--- transcodeVideos.sh
+++ transcodeVideos.sh
@@ -67,8 +67,6 @@ for file in "$downloadDirectory"/*.transcode.queue; do
         mv "$downloadDirectory/$videoId.ts" "$downloadDirectory/$videoId.ts.temp"
         mv "$downloadDirectory/$videoId.m3u8" "$downloadDirectory/$videoId.m3u8.temp"
 
-        rm "$downloadDirectory/$videoId.ts" "$downloadDirectory/$videoId.m3u8"
-
         rsync -avh "$downloadDirectory/$videoId.ts.temp" "$contentDirectory/$videoId.ts"
         rsync -avh "$downloadDirectory/$videoId.m3u8.temp" "$contentDirectory/$videoId.m3u8"
 

58bb956ab9242e654cfb062a1e79a3d30640d6ee
Trevor Sharp <sharp.trevor@me.com>
2023-10-11 17:25:28 +0000

    Update logging

--- src/services/contentService.ts
+++ src/services/contentService.ts
@@ -6,14 +6,12 @@ import { getQuality } from '../types/Quality';
 import { CONTENT_DIRECTORY } from '..';
 
 const getLocalFormats = async (videoId: string): Promise<VideoFormat[]> => {
-  const content = (await fs.promises.readdir(CONTENT_DIRECTORY)).filter((filename) =>
-    filename.includes(videoId)
-  );
+  const content = await fs.promises.readdir(CONTENT_DIRECTORY);
 
   console.log(content);
 
-  const videoFile = content.find((file) => file.includes('.ts'));
-  const playlistFile = content.find((file) => file.includes('.m3u8'));
+  const videoFile = content.find((file) => file.includes(videoId) && file.includes('.ts'));
+  const playlistFile = content.find((file) => file.includes(videoId) && file.includes('.m3u8'));
 
   if (!videoFile || !playlistFile) return [];
 
